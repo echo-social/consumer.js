e054865473f609f3f73b50990146ebf8604791fb
Stefan Izota <1038308+stefanizota@users.noreply.github.com>
2024-05-23 12:53:14 +0300

    Added send order.

--- index.js
+++ index.js
@@ -6,10 +6,10 @@ const ethers = require("ethers");
 import * as config from "./config.js";
 import fs from "fs";
 
-const ethersProvider = new ethers.JsonRpcProvider(config.NETWORK_RPC);
-const consumerAccount = new ethers.Wallet(config.WALLET_PRIVATE_KEY, ethersProvider);
+const provider = new ethers.providers.JsonRpcProvider(config.NETWORK_RPC);
+const consumer = new ethers.Wallet(config.WALLET_PRIVATE_KEY, provider);
 
-const chainId = parseInt(String((await ethersProvider.getNetwork()).chainId))
+const chainId = parseInt(String((await provider.getNetwork()).chainId))
 const oceanConfig = new oceanlib.ConfigHelper().getConfig(chainId);
 const oceanAddresses = JSON.parse(fs.readFileSync(config.ADDRESS_FILE, "utf8"))[config.NETWORK_NAME];
 
@@ -19,10 +19,51 @@ const asset = await aquarius.resolve(config.DID);
 console.info(asset);
 
 await oceanlib.approve(
-    consumerAccount,
+    consumer,
     oceanConfig,
-    consumerAccount.address,
+    consumer.address,
     oceanConfig.oceanTokenAddress,
     oceanConfig.fixedRateExchangeAddress,
     "100"
 );
+
+const initializeData = await oceanlib.ProviderInstance.initialize(
+    asset.id,
+    asset.services[0].id,
+    0,
+    consumer.address,
+    oceanConfig.providerUri
+);
+
+const providerFees = {
+    providerFeeAddress: initializeData.providerFee.providerFeeAddress,
+    providerFeeToken: initializeData.providerFee.providerFeeToken,
+    providerFeeAmount: initializeData.providerFee.providerFeeAmount,
+    v: initializeData.providerFee.v,
+    r: initializeData.providerFee.r,
+    s: initializeData.providerFee.s,
+    providerData: initializeData.providerFee.providerData,
+    validUntil: initializeData.providerFee.validUntil,
+};
+
+const datatoken = new oceanlib.Datatoken(consumer);
+
+const tx = await datatoken.startOrder(
+    oceanConfig.fixedRateExchangeAddress,
+    consumer.address,
+    0,
+    providerFees,
+);
+
+const orderTx = await tx.wait();
+const orderStartedTx = getEventFromTx(orderTx, "OrderStarted");
+
+const downloadURL = await oceanlib.ProviderInstance.getDownloadUrl(
+    asset.id,
+    asset.services[0].id,
+    0,
+    orderTx.transactionHash,
+    oceanConfig.providerUri,
+    consumer
+);
+console.info({ 'downloadURL': downloadURL });
